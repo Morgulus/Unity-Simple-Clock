f66168bf3c8e4bd29de2bea7b31611d6c7a9f9e2
Morgulus <em.krooot@gmail.com>
2017-07-05 20:09:40 +0300

    first commit

---
+++ Assets/ClockAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ClockAnimation : MonoBehaviour {
+
+    public Transform hours, minutes, seconds;
+    public bool analog;
+    private const float 
+        hoursRotation = 360 / 12f,
+        minutesRotation = 360 / 60f,
+        secondsRotation = 360 / 60f;
+	
+	// Update is called once per frame
+	void Update () {
+        if (analog)
+        {
+            TimeSpan timeSpan = DateTime.Now.TimeOfDay;
+            hours.localRotation = Quaternion.Euler(0f, 0f, (float)timeSpan.TotalHours * -hoursRotation);
+            minutes.localRotation = Quaternion.Euler(0f, 0f, (float)timeSpan.TotalMinutes * -minutesRotation);
+            seconds.localRotation = Quaternion.Euler(0f, 0f, (float)timeSpan.TotalSeconds * -secondsRotation);
+        }
+        else
+        {
+            DateTime time = DateTime.Now;
+            hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursRotation);
+            minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minutesRotation);
+            seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsRotation);
+        }
+    }
+}
